67ab1da392843d07c537c6db42462d9181de9e18
akira koga <akira4221@kki.biglobe.ne.jp>
2019-05-22 19:04:35 +0900

    tutorial 4.1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     assert_response :success
     # tilte タグ内に "(第二引数)"内の文字列があるか
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
