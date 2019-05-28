f7142d3b35df79d9005c2705cc4611d79e967dc1
akira koga <akira4221@kki.biglobe.ne.jp>
2019-05-28 16:58:42 +0900

    tutorial 5.3.3 (named root)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+
+  # ルーティングを変換
+  # get '/hoge', to: 'link' でhoge_pathやhoge_url(www含む全部)が定義なしで使える
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
 end
